d313d49e41ae0c85a8078187a9041a790027b632
league <league@imac-18.attlocal.net>
2019-06-01 13:00:46 -0700

    dfg

---
+++ src/body_part_quiz/Houses.java
@@ -0,0 +1,62 @@
+
+
+package body_part_quiz;
+
+
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class Houses {
+
+static Robot z = new Robot();	
+
+static int s = 60;		
+static int m = 120;
+static int l = 250;
+
+static void drawhouse(){
+	z.setPenColor(255,90,2);
+	z.move(m);		
+	z.turn(90);		
+	z.move(25);		
+	z.turn(90);
+	z.move(m);
+	z.turn(-90);
+	z.setPenColor(55,147,36);
+	z.move(10);		
+}
+
+static void drawPointy() {
+	z.setPenColor(66,131,237);
+	z.turn(-90);
+	z.move(s);		
+	z.turn(45);	
+	z.move(25);		
+	z.turn(90);
+	z.move(25);
+	z.turn(45);
+	z.move(s);
+	z.turn(-90);
+	z.setPenColor(55,147,36);
+	z.move(10);
+    z.turn(-90);
+		
+}
+	
+	public static void main(String[] args) {
+		
+
+
+	
+z.setX(0);
+z.penDown();
+z.setSpeed(999999999);
+
+
+for (int i = 0; i < 99; i++) {
+drawhouse();
+drawPointy();
+
+	}
+}
+}
